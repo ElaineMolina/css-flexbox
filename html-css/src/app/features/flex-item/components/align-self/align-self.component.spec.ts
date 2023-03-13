1379d1c14d2a80e0a7dcd060c567ec63361e42ee
ElaineMolina <elainemolina10@gmail.com>
2023-03-13 16:16:59 -0300

    feature flex-item

---
+++ html-css/src/app/features/flex-item/components/align-self/align-self.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlignSelfComponent } from './align-self.component';
+
+describe('AlignSelfComponent', () => {
+  let component: AlignSelfComponent;
+  let fixture: ComponentFixture<AlignSelfComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AlignSelfComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AlignSelfComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- html-css/src/app/features/flex-item/flex-item.module.ts
+++ html-css/src/app/features/flex-item/flex-item.module.ts
@@ -6,6 +6,9 @@ import { PageFlexItemComponent } from './pages/page-flex-item/page-flex-item.com
 import { FlexGrowComponent } from './components/flex-grow/flex-grow.component';
 import { FlexBasisComponent } from './components/flex-basis/flex-basis.component';
 import { FlexShrinkComponent } from './components/flex-shrink/flex-shrink.component';
+import { FlexComponent } from './components/flex/flex.component';
+import { OrderComponent } from './components/order/order.component';
+import { AlignSelfComponent } from './components/align-self/align-self.component';
 
 
 @NgModule({
@@ -13,7 +16,10 @@ import { FlexShrinkComponent } from './components/flex-shrink/flex-shrink.compon
     PageFlexItemComponent,
     FlexGrowComponent,
     FlexBasisComponent,
-    FlexShrinkComponent
+    FlexShrinkComponent,
+    FlexComponent,
+    OrderComponent,
+    AlignSelfComponent
   ],
   imports: [
     CommonModule,
